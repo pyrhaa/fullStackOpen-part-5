8ec216ef80492b278562aa0bf495f9194092e2ae
pyrhaa <sboubtane@protonmail.com>
2022-04-26 20:19:47 +0200

    add some modification andmy env var that you cannot see

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,12 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => {
+    console.log(response);
+    return response.data;
+  });
+};
 
-export default { getAll }
+export default { getAll };
