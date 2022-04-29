7d468874d5a3f3f8491e706247ec2cd205c78150
pyrhaa <sboubtane@protonmail.com>
2022-04-29 16:13:53 +0200

    put in async instead of promise then

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 const baseUrl = '/api/blogs';
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => {
-    console.log(response);
-    return response.data;
-  });
+const getAll = async () => {
+  const request = await axios.get(baseUrl);
+  return request.data;
 };
 
 export default { getAll };
