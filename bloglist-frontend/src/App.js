b8bf4077df927be6d4381ebc364d5e2e38214a20
pyrhaa <sboubtane@protonmail.com>
2022-04-29 16:00:58 +0200

    form ok

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -19,7 +19,27 @@ const App = () => {
   return (
     <div>
       <h2>log in to application</h2>
-      <form onSubmit={handleLog}></form>
+      <form onSubmit={handleLog}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
