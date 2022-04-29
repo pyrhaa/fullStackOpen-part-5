a44aa922cef010e8e9ab4a319a418d6a1d436fdd
pyrhaa <sboubtane@protonmail.com>
2022-04-29 15:58:03 +0200

    compose the login form

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -4,13 +4,22 @@ import blogService from './services/blogs';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  const handleLog = (e) => {
+    e.preventDefault();
+    console.log('logging in with', username, password);
+  };
+
   return (
     <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLog}></form>
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
