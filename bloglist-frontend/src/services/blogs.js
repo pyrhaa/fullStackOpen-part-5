07ec50f67cbfa26d65941829ccb27f4705bafb1a
pyrhaa <sboubtane@protonmail.com>
2022-05-16 18:37:21 +0200

    detele services done

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -26,4 +26,13 @@ const update = async (id, newObject) => {
   return response.data;
 };
 
-export default { getAll, create, update, setToken };
+const deletes = async (id, deleteObject) => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.delete(`${baseUrl}/${id}`, deleteObject, config);
+  return response.data;
+};
+
+export default { getAll, create, update, deletes, setToken };
