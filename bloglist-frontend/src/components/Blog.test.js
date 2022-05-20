b94f1edcbf9ae0618ffb2dae0652c3fd31eafc68
pyrhaa <sboubtane@protonmail.com>
2022-05-20 11:44:07 +0200

    5.13 step1 in progress

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -4,4 +4,21 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
-test('', () => {});
+describe('<Blog />', () => {
+  let container;
+
+  const blog = {
+    title: 'Tamazgha: the real north Africa',
+    author: 'Yan Amazigh',
+    url: 'www.tamaz.tm',
+    likes: 10000000000
+  };
+
+  const mockHandler = jest.fn();
+
+  beforeEach(() => {
+    container = render(<Blog blog={blog} likes={mockHandler} />);
+  });
+
+  test('render title & author, not url & likes', () => {});
+});
