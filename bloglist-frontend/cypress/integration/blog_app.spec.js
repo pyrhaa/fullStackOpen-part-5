475f967f9f3d7803404de76c41229a1a5d753418
pyrhaa <sboubtane@protonmail.com>
2022-05-22 12:20:58 +0200

    5.17 step 1 in progress

--- bloglist-frontend/cypress/integration/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,14 @@
 describe('Blog app', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:3000');
+  });
+  it('front page can be opened', function () {
     cy.contains('log in to application');
+  });
+  it('user can log in', function () {
+    cy.contains('login').click();
+    cy.get('#username').type('user');
+    cy.get('#password').type('userpassword');
+    cy.get('#login-button').click();
   });
 });
--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -118,6 +118,7 @@ const App = () => {
           <div>
             username
             <input
+              id="username"
               type="text"
               value={username}
               name="Username"
@@ -127,13 +128,16 @@ const App = () => {
           <div>
             password
             <input
+              id="password"
               type="password"
               value={password}
               name="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
-          <button type="submit">login</button>
+          <button id="login-button" type="submit">
+            login
+          </button>
         </form>
       </div>
     );
